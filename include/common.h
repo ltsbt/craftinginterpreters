1af3c2418deff26f418347d3b15975ccf3211095
ltsbt <ltsbt@proton.me>
2024-06-06 14:43:44 +0200

    chapter 15

--- include/common.h
+++ include/common.h
@@ -1,8 +1,10 @@
-#ifndef clox_common_h
-#define clox_common_h
+#ifndef CLOX_COMMON_H
+#define CLOX_COMMON_H
 
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+
+#define DEBUG_TRACE_EXECUTION
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,15 +1,35 @@
 #include "chunk.h"
 #include "common.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(void) {
+    init_vm();
     Chunk chunk;
     init_chunk(&chunk);
     int constant = add_constant(&chunk, 1.2);
     write_chunk(&chunk, OP_CONSTANT, 123);
     write_chunk(&chunk, constant, 123);
+
+    constant = add_constant(&chunk, 3.4);
+    write_chunk(&chunk, OP_CONSTANT, 123);
+    write_chunk(&chunk, constant, 123);
+
+    write_chunk(&chunk, OP_ADD, 123);
+
+    constant = add_constant(&chunk, 5.6);
+    write_chunk(&chunk, OP_CONSTANT, 123);
+    write_chunk(&chunk, constant, 123);
+
+    write_chunk(&chunk, OP_DIVIDE, 123);
+
+    write_chunk(&chunk, OP_NEGATE, 123);
+
     write_chunk(&chunk, OP_RETURN, 123);
+
     disassemble_chunk(&chunk, "test chunk");
+    interpret(&chunk);
+    free_vm();
     free_chunk(&chunk);
     return 0;
 }
