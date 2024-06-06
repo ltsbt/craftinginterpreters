c60f370bea267d934f6d855bd792d9c5aee19074
ltsbt <ltsbt@proton.me>
2024-06-07 00:16:13 +0200

    chapter 16

--- include/memory.h
+++ include/memory.h
@@ -3,12 +3,9 @@
 
 #include "common.h"
 
-#define GROW_CAPACITY(capacity) ((capacity) < 8 ? 8 : (capacity) * 2)
-#define GROW_ARRAY(type, pointer, oldCount, newCount)                          \
-    (type*)reallocate(pointer, sizeof(type) * (oldCount),                      \
-                      sizeof(type) * (newCount))
-#define FREE_ARRAY(type, pointer, old_count)                                   \
-    reallocate(pointer, sizeof(type) * (old_count), 0)
+#define GROW_CAPACITY(capacity)                       ((capacity) < 8 ? 8 : (capacity) * 2)
+#define GROW_ARRAY(type, pointer, oldCount, newCount) (type*)reallocate(pointer, sizeof(type) * (oldCount), sizeof(type) * (newCount))
+#define FREE_ARRAY(type, pointer, old_count)          reallocate(pointer, sizeof(type) * (old_count), 0)
 
 void* reallocate(void* pointer, size_t old_size, size_t new_size);
 
