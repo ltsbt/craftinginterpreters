31042c38babfd70b604815cf0ad6afaefe4f5fad
ltsbt <ltsbt@proton.me>
2024-06-11 04:47:11 +0200

    chapter 18

--- src/value.c
+++ src/value.c
@@ -14,8 +14,7 @@ void write_value_array(ValueArray* array, Value value) {
     if (array->capacity < array->count + 1) {
         int old_capacity = array->capacity;
         array->capacity  = GROW_CAPACITY(old_capacity);
-        array->values =
-            GROW_ARRAY(Value, array->values, old_capacity, array->capacity);
+        array->values    = GROW_ARRAY(Value, array->values, old_capacity, array->capacity);
     }
     array->values[array->count] = value;
     array->count++;
@@ -26,4 +25,21 @@ void free_value_array(ValueArray* array) {
     init_value_array(array);
 }
 
-void print_value(Value value) { printf("%g", value); }
+void print_value(Value value) {
+    switch (value.type) {
+        case VAL_BOOL  : printf(AS_BOOL(value) ? "true" : "false"); break;
+        case VAL_NIL   : printf("nil"); break;
+        case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
+    }
+}
+
+bool values_equal(Value a, Value b) {
+    if (a.type != b.type) return false;
+
+    switch (a.type) {
+        case VAL_BOOL  : return AS_BOOL(a) == AS_BOOL(b);
+        case VAL_NIL   : return true;
+        case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
+        default        : return false;
+    }
+}
